06a29664df0cfe7110348be37f2d5c0cf2b79766
Omar Syed <osyed1@gmail.com>
2025-04-04 18:48:09 -0500

    Delete src directory

--- src/pages/simulationsNew.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SimulationsNew from '../components/components/SimulationsNew.js';
-
-export default function Simulation() {
-  return (<> < div className = "flex-row content-center items-center text-xl text-stone-300" > <SimulationsNew/>
-</div>
-</>)
-}
